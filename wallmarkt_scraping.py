48cd3d997e03c8b4c8c9540eb96097543ba50d34
Dusan Rangelov <dusanrangelov@yahoo.com>
2025-03-19 15:44:17 +0100

    feature: extracted the main product fields

--- wallmarkt_scraping.py
+++ wallmarkt_scraping.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 walmart_ur = "https://www.walmart.com/ip/LG-27-Color-Smart-Green-27SR50F-G-AUS/5465289649?adsRedirect=true"
 
@@ -12,9 +13,28 @@ HEADERS = {
 
 response = requests.get(walmart_ur, headers=HEADERS)
 
+#use web token counter to find the value for the price or import json lib
 soup = BeautifulSoup(response.text, "html.parser")
 script_tag = soup.find("script", id="__NEXT_DATA__")
 
-html = script_tag
+data = json.loads(script_tag.string)
+initial_data = data["props"]["pageProps"]["initialData"]["data"]
+product_data = initial_data["product"]
+reviews_data = initial_data.get("reviews", {})
 
-print(html)
+
+product_info = {
+                "price": product_data["priceInfo"]["currentPrice"]["price"],
+                "review_count": reviews_data.get("totalReviewCount", 0),
+                "item_id": product_data["usItemId"],
+                "avg_rating": reviews_data.get("averageOverallRating", 0),
+                "product_name": product_data["name"],
+                "brand": product_data.get("brand", ""),
+                "availability": product_data["availabilityStatus"],
+                "image_url": product_data["imageInfo"]["thumbnailUrl"],
+                "short_description": product_data.get("shortDescription", "")
+            }
+
+#html = script_tag
+
+print(product_info)
