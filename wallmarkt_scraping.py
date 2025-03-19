40c1237c591bb7f6db38da431b475ce29c7610cf
Dusan Rangelov <dusanrangelov@yahoo.com>
2025-03-19 23:32:13 +0100

    First woring version

--- wallmarkt_scraping.py
+++ wallmarkt_scraping.py
@@ -2,8 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-walmart_ur = "https://www.walmart.com/ip/LG-27-Color-Smart-Green-27SR50F-G-AUS/5465289649?adsRedirect=true"
-
 HEADERS = {
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
     "accept": "application/json",
@@ -11,19 +9,47 @@ HEADERS = {
     "accept-encoding": "gzip, deflate, br, zstd",
 }
 
+site_url = "https://www.walmart.com"
+
+
+def get_product_lnk(query, page_num=1):
+    search_template_url = f"https://www.walmart.com/search?q={query}&page={page_num}"
+    response = requests.get(search_template_url, headers=HEADERS)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = soup.find_all('a', href=True)
+
+    product_links = []
+
+    for a_tag in links:
+        if '/ip/' in a_tag['href']:
+            found = True
+            if "https" in a_tag['href']:
+                complete_url = a_tag['href']
+                product_links.append(complete_url)
+            else:
+                complete_url = site_url + a_tag['href']
+                product_links.append(complete_url)
+            if not found:
+                print("\n\nSoup not found", soup)
+
+    return product_links
+
 
 def extract_prod_info(product_url):
     response = requests.get(product_url, headers=HEADERS)
 
-    #use web token counter to find the value for the price or import json lib
+    # use web token counter to find the value for the price or import json lib
     soup = BeautifulSoup(response.text, "html.parser")
     script_tag = soup.find("script", id="__NEXT_DATA__")
+
+    if script_tag is None:
+        return None
 
     data = json.loads(script_tag.string)
     initial_data = data["props"]["pageProps"]["initialData"]["data"]
     product_data = initial_data["product"]
     reviews_data = initial_data.get("reviews", {})
-
 
     product_info = {
                     "price": product_data["priceInfo"]["currentPrice"]["price"],
@@ -37,6 +63,29 @@ def extract_prod_info(product_url):
                     "short_description": product_data.get("shortDescription", "")
                 }
 
-    #html = script_tag
-
     return product_info
+
+
+def main():
+    output_file = "p_info.json"
+    with open(output_file, "w") as file:
+        page_num = 1
+        while True:
+            links = get_product_lnk("monitors", page_num)
+            if not links or page_num > 1:
+                break
+
+            for link in links:
+                try:
+                    product_info = extract_prod_info(link)
+                    if product_info:
+                        file.write(json.dumps(product_info)+"\n")
+                except Exception as e:
+                    print(f"Process failed for URL: {link}. \n Error: {e}")
+            page_num += 1
+            print(f"Searched page {page_num}")
+
+
+
+if __name__ == "__main__":
+    main()
